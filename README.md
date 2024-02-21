b37fe57b4290bd0f32a0850a2e96de112314854e
Umu Hawa Bah <bahumuhawa96@gmail.com>
2024-02-21 14:24:10 +0100

    expense tracker

---
+++ README.md
@@ -0,0 +1,21 @@
+# vueSpend - Expense Tracker
+
+Introducing an expense tracker app developed with Vue 3 and the composition API. Features include:
+
+- Seamless addition and removal of expenses/income.
+- Real-time tracking of balance.
+- Data persistence via local storage.
+- Integration of Vue Toastification for notifications.
+- Utilization of the <script setup> syntax (Vue 3.2+).
+
+## Project Setup
+
+npm install
+
+### Compile and Hot-Reload for Development
+
+npm run dev
+
+### Compile and Minify for Production
+
+npm run build
---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+ <Header />
+ <div class="container">
+  <Balance :total="total" />
+  <IncomeExpenses :income="+income" :expenses="+expenses" />
+  <TransactionList :transactions="transactions" @transactionsDeleted="handleTransactionDeleted" />
+  <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
+ </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import { ref, computed, onMounted } from 'vue';
+import { useToast } from 'vue-toastification';
+
+const toast = useToast();
+
+const transactions = ref([]);
+
+onMounted(() => {
+ const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+ if (savedTransactions) {
+  transactions.value = savedTransactions;
+ }
+});
+
+//Get Total
+const total = computed(() => {
+ return transactions.value.reduce((acc, transaction) => {
+  return acc + transaction.amount
+ }, 0);
+});
+
+//Get income
+const income = computed(() => {
+ return transactions.value
+  .filter((transaction) => transaction.amount > 0)
+  .reduce((acc, transaction) => acc + transaction.amount, 0)
+  .toFixed(2)
+})
+
+//Get expenses
+const expenses = computed(() => {
+ return transactions.value
+  .filter((transaction) => transaction.amount > 0)
+  .reduce((acc, transaction) => acc + transaction.amount, 0)
+  .toFixed(2)
+})
+
+//Submit transaction
+const handleTransactionSubmitted = (transactionData) => {
+ transactions.value.push({
+  id: generateUniqueId(),
+  text: transactionData.text,
+  amount: transactionData.amount
+ });
+
+ savedTransactionsToLocalStorage();
+ toast.success('Transaction added')
+};
+
+//Generate unique ID
+const generateUniqueId = () => {
+ return Math.floor(Math.floor() * 1000000);
+};
+
+//Delete Transaction
+const handleTransactionDeleted = (id) => {
+ transactions.value = transactions.value.filter(
+  (transaction) => transaction.id !== id
+ );
+
+ savedTransactionsToLocalStorage();
+
+ toast.success('Transaction deleted');
+
+};
+
+//Save transactions to local storage
+const savedTransactionsToLocalStorage = () => {
+ localStorage.setItem('transactions', JSON.stringify(transactions.value));
+};
+</script>
